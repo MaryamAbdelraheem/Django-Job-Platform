2e6d6644c46cfce38e073857ab9c70ba45b71d68
Maryam Abdelraheeem <maryamabldelraheem86@gmail.com>
2024-04-14 17:55:01 +0200

    design job models & add admin

--- analysis.md
+++ analysis.md
@@ -2,17 +2,17 @@ job :
     -title --
     -location --
     -creation_date --
-
+    -salary_range
+    -description
+    -vacancy
+    -job_nature [full_time, part_time, remote,  freelance ]
+    -experince
 company:
     -company_name
     -company_logo
     -company_subtitle
     -company_website
     -company_email
-    -salary_range
-    -description
-    -vacancy
-    -job_nature [full_time, part_time, remote,  freelance ]
     -experince
 
 category:
--- job/models.py
+++ job/models.py
@@ -1,11 +1,30 @@
 from django.db import models
 from django_countries.fields import CountryField
 from django.utils import timezone
+
+#tuples of tuples ; one stored at databse & one appeare to the user
+JOB_TYPE = (
+    ('full_time','full_time'),
+    ('part_time','part_time'),
+    ('remote','remote'),
+    ('freelance','freelance'),
+)
+
+
 class Job(models.Model):
     title = models.CharField(max_length=120)
     location = CountryField()
     create_at = models.DateTimeField( default=timezone.now)
     company = models.ForeignKey('Company', on_delete=models.CASCADE, related_name ='job_company')
+    salary_start = models.IntegerField(null=True, blank=True)
+    salary_end = models.IntegerField(null=True, blank=True )
+    description = models.TextField(max_length= 15000)
+    vacancy =  models.IntegerField()
+    job_type = models.CharField(choices=JOB_TYPE, max_length=10)
+    experince = models.IntegerField()
+    category = models.ForeignKey('Category', related_name='job_category', on_delete=models.SET_NULL, null=True, blank=True )
+    def __str__(self):
+        return self.title 
 
 class Company(models.Model):
     name = models.CharField(max_length=50)
@@ -13,6 +32,11 @@ class Company(models.Model):
     subtitle = models.TextField(max_length=1000)
     website = models.URLField()
     email = models.EmailField()
+    def __str__(self):
+        return self.name 
 
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=30)
+    logo = models.CharField(max_length=30)
+    def __str__(self):
+        return self.name  
